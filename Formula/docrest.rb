cc3714e4bf8d64eb5fec89495be0ae59bd296328
Jesús R Peinado <jesus.peinado@garajedeideas.com>
2026-05-14 16:43:17 +0200

    feat(): docrest alive

--- Formula/docrest.rb
+++ Formula/docrest.rb
@@ -18,7 +18,7 @@ class Docrest < Formula
 
   desc "Convert documentation between formats (md, docx, pdf, txt, diagrams)"
   homepage "https://github.com/jesusr/docrest"
-  url "https://github.com/jesusr/docrest/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://github.com/jesusr/docrest/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
