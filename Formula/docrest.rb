f9f0e82930d58ecd0d5036370657f086ff8f1df2
Jesús R Peinado <jesus.peinado@garajedeideas.com>
2026-05-15 09:46:41 +0200

    Initialize brew

--- Formula/docrest.rb
+++ Formula/docrest.rb
@@ -17,8 +17,8 @@ class Docrest < Formula
   include Language::Python::Virtualenv
 
   desc "Convert documentation between formats (md, docx, pdf, txt, diagrams)"
-  homepage "https://github.com/REPLACE_ME/docrest"
-  url "https://github.com/REPLACE_ME/docrest/archive/refs/tags/v0.1.0.tar.gz"
+  homepage "https://github.com/jesusr/docrest"
+  url "https://github.com/jesusr/docrest/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
